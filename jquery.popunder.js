16c5f876af64049af8b0b7bdf404e451a02fada3
Hans-Peter Buniat <hpbuniat@gmail.com>
2012-10-01 19:41:17 +0200

    some cleanup

--- jquery.popunder.js
+++ jquery.popunder.js
@@ -10,34 +10,33 @@
  * @requires jQuery
  */
 (function($) {
+    "use strict";
+
     /**
      * Create a popunder
      *
      * @param  sUrl Url to open as popunder
-     * @param  array aOptions Options for the Popunder
+     * @param  aOptions Options for the Popunder
      *
      * @return jQuery
      */
     $.popunder = function(sUrl, aOptions) {
-        use "strict";
         aOptions = aOptions || {};
-        aOptions.simple = $.browser.msie;
-        $.popunderHelper.open(sUrl, aOptions);
+        $.popunder.helper.open(sUrl, aOptions);
         return $;
     };
 
     /* several helper functions */
-    $.popunderHelper = {
+    $.popunder.helper = {
         /**
          * Helper to create a (optionally) random value with prefix
          *
-         * @param  string sUrl The url to open
-         * @param  array aOptions Options for the Popunder
+         * @param  sUrl The url to open
+         * @param  aOptions Options for the Popunder
          *
          * @return boolean
          */
         cookieCheck: function(sUrl, aOptions) {
-            use "strict";
             var name = this.rand(aOptions.cookie, false),
                 cookie = $.cookies.get(name),
                 ret = false;
@@ -62,8 +61,8 @@
         /**
          * Helper to create a (optionally) random value with prefix
          *
-         * @param  string name
-         * @param  boolean rand
+         * @param  name
+         * @param  rand
          *
          * @return string
          */
@@ -75,27 +74,27 @@
         /**
          * Open the popunder
          *
-         * @param  string sUrl The URL to open
-         * @param  array aOptions
+         * @param  sUrl The URL to open
+         * @param  aOptions
          *
          * @return boolean
          */
         open: function(sUrl, aOptions) {
-            var _parent = self,
+            var _parent = window.self,
                 sToolbar = (!$.browser.webkit && (!$.browser.mozilla || parseInt($.browser.version, 10) < 12)) ? 'yes' : 'no',
                 sOptions,
                 popunder;
 
             aOptions.blocktime = (aOptions.blocktime) ? aOptions.blocktime : false;
             aOptions.cookie = (aOptions.cookie) ? aOptions.cookie : 'puCookie';
-            aOptions.height = (aOptions.height) ? aOptions.height : (screen.availHeight - 122).toString(),
+            aOptions.height = (aOptions.height) ? aOptions.height : (screen.availHeight - 122).toString();
             aOptions.width = (aOptions.width) ? aOptions.width : (screen.availWidth - 122).toString();
 
-            if (aOptions.blocktime && $.popunderHelper.cookieCheck(sUrl, aOptions)) {
+            if (aOptions.blocktime && $.popunder.helper.cookieCheck(sUrl, aOptions)) {
                 return false;
             }
 
-            if (top != self) {
+            if (top !== window.self) {
                 try {
                     if (top.document.location.toString()) {
                         _parent = top;
@@ -108,29 +107,31 @@
             sOptions = 'toolbar=' + sToolbar + ',scrollbars=yes,location=yes,statusbar=yes,menubar=no,resizable=1,width=' + aOptions.width + ',height=' + aOptions.height + ',screenX=0,screenY=0,left=0,top=0';
 
             /* create pop-up from parent context */
-            popunder = _parent.window.open(sUrl, $.popunderHelper.rand(), sOptions);
+            popunder = _parent.window.open(sUrl, $.popunder.helper.rand(), sOptions);
             if (popunder) {
                 popunder.blur();
-                if (aOptions.simple) {
-                    /* classic popunder, used for ie*/
+                if ($.browser.msie === true) {
+                    /* classic popunder, used for ie */
                     window.focus();
-                    try { opener.window.focus(); }
-                    catch (err) { }
+                    try {
+                        opener.window.focus();
+                    }
+                    catch (err) {
+                    }
                 }
                 else {
-                    /* popunder for e.g. ff4+, chrome */
+                    /* popunder for e.g. ff, chrome */
                     popunder.init = function(e) {
-                        with (e) {
-                            (function() {
-                                if (typeof window.mozPaintCount !== 'undefined' || typeof navigator.webkitGetUserMedia === "function") {
-                                    var x = window.open('about:blank');
-                                    x.close();
-                                }
-
-                                try { opener.window.focus(); }
-                                catch (err) { }
-                            })();
+                        /* in ff4+, chrome21+ we need to trigger a window.open on our parent to bring it to the front */
+                        if (typeof e.window.mozPaintCount !== 'undefined' || typeof e.navigator.webkitGetUserMedia === "function") {
+                            var x = e.window.open('about:blank');
+                            x.close();
                         }
+
+                        try {
+                            e.opener.window.focus();
+                        }
+                        catch (err) { }
                     };
                     popunder.params = {
                         url: sUrl
