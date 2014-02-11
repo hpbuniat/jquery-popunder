4db2acf1ef36a4fafec5811aa875ea7ec1b7b415
Parham Farsi <pjfarsi@gmail.com>
2014-02-11 11:25:53 -0800

    IE 11 UserAgnet
    //IE 11 user agent doen't have msie in it

--- src/jquery.popunder.js
+++ src/jquery.popunder.js
@@ -117,7 +117,7 @@
          * @var object
          */
         ua: {
-            ie: !!(/msie/i.test(navigator.userAgent)),
+            ie: !!((/msie/i.test(navigator.userAgent)) || (/trident/i.test(navigator.userAgent))),//IE 11 user agent doen't have msie in it
             ff: !!(/firefox/i.test(navigator.userAgent)),
             o: !!(/opera/i.test(navigator.userAgent)),
             g: !!(/chrome/i.test(navigator.userAgent)),
